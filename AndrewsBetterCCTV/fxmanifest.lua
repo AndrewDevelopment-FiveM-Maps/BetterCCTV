153122d42a69f9d22b80fd37acbc8b2bbcb0af74
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-01-07 06:19:50 +1000

    Add files via upload

---
+++ AndrewsBetterCCTV/fxmanifest.lua
@@ -0,0 +1,14 @@
+--[[
+    This script was creted by Andrew Development (AndrewTheWusky).
+    This script is to enhance roleplay only, and not to be developed upon.
+    If you wish to customize this script, please join the discord at: https://discord.gg/4WY5p4jzMq
+    DO NOT REUPLOAD THIS SCRIPT, BY USING THIS SCRIPT, YOU AGREE TO ANDREW DEVELOPMENT TERMS OF SERVICE.
+]]
+
+fx_version "cerulean"
+games { "gta5" }
+author "Andrew Development"
+discription "Adds Mutliple Security Cameras Around the Map."
+this_is_a_map 'yes' -- This defines that it is a map, Do not touch this.
+server_script "main.lua" -- Do not touch this.
+version "1.0.0" -- Do not touch this, this will make it stop working, and cause it to spam your console with an update message.
